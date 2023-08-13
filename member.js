488c7bcbeec14f43f9d8c15bb28e1ecee27ffd1e
Akshay Hebbar <AkshayHebbar@users.noreply.github.com>
2023-08-13 21:55:43 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var skills = document.getElementById("skills").value;
+    var skillsError = document.getElementById("skillsError");
+    if (skills == "") {
+        skillsError.innerHTML = "Please enter your skills";
+        skillsError.style.color = "red";
+        return false;
+    }
+    else {
+        skillsError.innerHTML = "";
+        return true;
+    }
+}
